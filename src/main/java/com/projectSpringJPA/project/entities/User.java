5a3ab4c4f2c34a9c5c1bc4fcb5854fae4b423c45
FernandoJMartins <julio8martins@gmail.com>
2024-04-03 12:05:46 -0300

    db seeding and dependecy injection

--- src/main/java/com/projectSpringJPA/project/entities/User.java
+++ src/main/java/com/projectSpringJPA/project/entities/User.java
@@ -3,10 +3,24 @@ package com.projectSpringJPA.project.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column //é necessario?
 	private String name;
 	private String email;
 	private String phone;
